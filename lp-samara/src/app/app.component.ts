6ab6ef33a12c459fa7aee184d2c785bf91ebdc99
Guilherme Falcão <guifalcao2017@gmail.com>
2025-03-14 23:12:32 -0300

    Creation of header, hero and features components

--- lp-samara/src/app/app.component.ts
+++ lp-samara/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
+import { HeroComponent } from '../components/hero/hero.component';
+import { FeaturesComponent } from '../components/features/features.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, FeaturesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ lp-samara/src/components/features/features.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-features',
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './features.component.html',
+  styleUrl: './features.component.scss'
+})
+export class FeaturesComponent {
+
+}
